089b83c8da39b38bd2e27a6acadb79c5432e225e
obisopulu <obisopulu@gmail.com>
2024-03-07 01:00:14 +0100

    Add hex and rgba converter

--- src/components/ColorGenerator.js
+++ src/components/ColorGenerator.js
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
+import './style.css';
 
 const Wrapper = styled.section`
   display: flex;
@@ -24,13 +25,24 @@ const Description = styled.div`
   padding-bottom: 1em;
 `;
 const Body = styled.div`
+  align-items: stretch;
   display: grid;
   grid-template-columns: 46% 50%;
   gap: 10px;
   font-size: 13px;
+  padding: 0 50px;
+`;
+const BodyHRC = styled.div`
+  align-items: stretch;
+  display: grid;
+  grid-template-columns: 46% auto;
+  gap: 10px;
+  font-size: 13px;
+  padding: 0 50px;
+  justify-content: space-between
 `;
 const ColorPad = styled.div`
-  width: 100%;
+  width: 98%;
   border-radius: 10px;
   border: 3px solid #ccc;
   box-shadow: 0 0 10px rgba(0,0,0,0.3);
@@ -39,6 +51,7 @@ const ColorPad = styled.div`
   align-items: center;
   color: #FFF;
   font-weight: bold;
+  padding: 20px 0;
   text-shadow: 0 0 4px rgba(0, 0, 0, 0.5);
 `;
 const Button = styled.button`
@@ -57,11 +70,24 @@ const Button = styled.button`
     box-shadow: 0 0 1px rgba(0,0,0,0.1);
   }
 `;
+const Input = styled.input`
+  background: #fff;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0,0,0,0.1);
+  &:hover {
+    background: #f7f7f7;
+  }
+`;
 const ColorGenerator = () => {
   const colorPad = useRef(null);
+  const colorConverter = useRef(null);
 
   const [color, setColor] = useState('#f0f0f0');
   const [type, setType] = useState('hex');
+  const [convertColor, setConvertColor] = useState('#f0f0f0');
+  const [convertedColor, SetConvertedColor] = useState('#f0f0f0');
 
   const colorType = (type) => {
     setType(type);
@@ -83,6 +109,44 @@ const ColorGenerator = () => {
      }
      colorPad.current.style.backgroundColor = color;
   }
+
+  const convertType = () => {
+    if(convertColor.length <= 7) {
+      
+      let hex = convertColor.replace('#', '');
+    
+      const r = parseInt(hex.substring(0, 2), 16);
+      const g = parseInt(hex.substring(2, 4), 16);
+      const b = parseInt(hex.substring(4, 6), 16);
+    
+      SetConvertedColor(`rgba(${r}, ${g}, ${b}, 1)`);
+    }else{
+        const rgbaValues = convertColor.match(/\d+/g);
+      
+        const r = parseInt(rgbaValues[0]);
+        const g = parseInt(rgbaValues[1]);
+        const b = parseInt(rgbaValues[2]);
+      
+        const a = parseFloat(rgbaValues[3]);
+      
+        const componentToHex = (c) => {
+          const hex = c.toString(16);
+          return hex.length === 1 ? '0' + hex : hex;
+        };
+      
+        const hex = '#' + componentToHex(r) + componentToHex(g) + componentToHex(b);
+      
+        if (a !== 1) {
+          const alphaHex = Math.round(a * 255).toString(16);
+          return hex + componentToHex(alphaHex);
+        }
+      
+        SetConvertedColor(hex);      
+    }
+
+    colorConverter.current.style.backgroundColor = convertedColor;
+  }
+
   return (
     <Wrapper>
       <Header>
@@ -92,21 +156,38 @@ const ColorGenerator = () => {
         <Description>
           Click the button below to generate a random color. You can use the color for your next project.
         </Description>
-        <Body>
-          <Button className='item1' onClick={() => colorType('hex')}>
-            Hex
-          </Button>
-          <Button className='item2' onClick={() => colorType('rgba')}>
-            RGBA
-          </Button>
-          <Button className='item3' onClick={() => generateColor()}>
-            Generate Color
-          </Button>
-          <ColorPad className='item4' ref={colorPad}>
-            {color}
-          </ColorPad>
-        </Body>
       </Header>
+      <Body>
+        <Button className='RCG-item1' onClick={() => colorType('hex')}>
+          Hex
+        </Button>
+        <Button className='RCG-item2' onClick={() => colorType('rgba')}>
+          RGBA
+        </Button>
+        <Button className='RCG-item3' onClick={() => generateColor()}>
+          Generate Color
+        </Button>
+        <ColorPad className='RCG-item4' ref={colorPad}>
+          {color}
+        </ColorPad>
+      </Body>
+      <Header>
+        <Title>
+          HEX - RGBA Converter
+        </Title>
+        <Description>
+          Enter a HEX or RGBA color code to convert it to the other format.
+        </Description>
+      </Header>
+      <BodyHRC>
+        <Input className='' type='text' placeholder='#000000 or rgba(0,0,0,0)' onChange={(e) => setConvertColor(e.target.value)}/>
+        <Button className='' onClick={() => convertType()}>
+          Generate Color
+        </Button>
+        <ColorPad className='HRC-item' ref={colorConverter}>
+          {convertedColor}
+        </ColorPad>
+      </BodyHRC>
     </Wrapper>
   )
 }
